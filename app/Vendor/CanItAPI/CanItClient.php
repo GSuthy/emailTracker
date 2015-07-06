43a873ae40376b86112e0c86a372a0410262ed72
michaeljmatthews <michaeljmatthews@byu.edu>
2015-07-06 16:16:32 -0600

    Changing count, and query in Health Controller

--- app/Vendor/CanItAPI/CanItClient.php
+++ app/Vendor/CanItAPI/CanItClient.php
@@ -117,7 +117,7 @@ class CanItClient {
         $canit_url = "https://emailfilter.byu.edu/canit/api/2.0";
         $api = new CanItAPIClient($canit_url);
         $success = $api->login(settings::$credentials['username'], settings::$credentials['password']);
-        $results = $api->do_get("log/search/1/count?query");
+        $results = $api->do_get("log/search/1/40?sender=michaeljmatthews@byu.edu");
 
         if (!$api->succeeded()) {
             print "GET request failed: " . $api->get_last_error() . "\n";
