16d83b12f9b145ea9128dc53810ab62578d3a0d7
michaeljmatthews <michaeljmatthews@byu.edu>
2015-07-08 14:12:34 -0600

    changing hostname to message

--- app/Controller/HealthController.php
+++ app/Controller/HealthController.php
@@ -45,9 +45,10 @@ class HealthController extends AppController {
 
         $message = CanItClient::searchlog();
         foreach ($message as $temp) {
-            if ($temp['message'] == "All mounted volumes have at least 10% free disk space and inodes" && $temp['test_ok'] == 1 && $temp['hostname'] == "gw10.byu.edu" || "gw5.byu.edu" || "gw3.byu.edu") {
+            if ($temp['message'] == "All mounted volumes have at least 10% free disk space and inodes"){
+                // && $temp['test_ok'] == 1 && $temp['hostname'] == "gw10.byu.edu" || "gw5.byu.edu" || "gw3.byu.edu") {
         
-                echo "working ";
+                echo "working";
             }
            
         }
