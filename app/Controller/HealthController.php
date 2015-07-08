221830311531ae75b6075fd81e6b3ca5e739c41c
michaeljmatthews <michaeljmatthews@byu.edu>
2015-07-08 14:19:59 -0600

    making changes to data format

--- app/Controller/HealthController.php
+++ app/Controller/HealthController.php
@@ -55,7 +55,7 @@ class HealthController extends AppController {
         print_r($searchLogs);
         echo "</pre>";
         exit();
-        
+$array = array();      
 $array[] = "item";
 $array[$key] = "item";
 array_push($array, "item", "another item");
